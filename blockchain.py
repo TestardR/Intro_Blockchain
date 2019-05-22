f4d0578cb82badb0cf2636cc8dbc689e39c84d5e
unknown <rmtestard@gmail.com>
2019-05-22 20:42:53 +0200

    add conditonals

--- blockchain.py
+++ blockchain.py
@@ -1,3 +1,4 @@
+# Initializing our blockchain list
 blockchain = []
 
 
@@ -9,20 +10,34 @@ def add_value(transaction_amount, last_transaction=[1]):
     blockchain.append([last_transaction, transaction_amount])
 
 
-def get_user_input():
+def get_transaction_value():
     return float(input('Your transaction amount please: '))
 
 
-tx_amount = get_user_input()
+def get_user_choice():
+    user_input = input('Your choice: ')
+    return user_input
+
+
+def print_blockchain_elements():
+    for block in blockchain:
+        print(block)
+
+
+tx_amount = get_transaction_value()
 add_value(tx_amount)
 
-tx_amount = get_user_input()
-add_value(tx_amount, get_last_blockchain())
+while True:
+    print('Please choose')
+    print('1: Add a new transaction value')
+    print('2: Output the blockchain blocks')
+    user_choice = get_user_choice()
+    if user_choice == '1':
+        tx_amount = get_transaction_value()
+        add_value(tx_amount, get_last_blockchain())
+    else:
+        print_blockchain_elements()
+        # print(blockchain)
 
-tx_amount = get_user_input()
-add_value(tx_amount, get_last_blockchain())
 
-tx_amount = get_user_input()
-add_value(tx_amount, get_last_blockchain())
-
-print(blockchain)
+print('done')
